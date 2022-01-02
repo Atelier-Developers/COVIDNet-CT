ec4cd438fd8f6e80086b6f49ef5bcfbbefbd38f6
amirbaghi <bamirmasoud@gmail.com>
2022-01-02 22:17:56 +0330

    add flair service to docker-compose

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 build=$1
 mode=$2
 sudo rm -rf multiprocess_metrics/*.db
-export DISPLAY="$(grep nameserver /etc/resolv.conf | sed 's/nameserver //'):0"
+#export DISPLAY="$(grep nameserver /etc/resolv.conf | sed 's/nameserver //'):0"
 if [ "$build" = "build" ];
 then
 docker-compose -f docker-compose.dev.yml -f docker-compose.yml up --build -d
